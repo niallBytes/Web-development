349ed1738849505d0f3e339daecf5796372a9dbd
niallBytes <unaniniall3@gmail.com>
2026-06-19 16:59:56 +0530

    1.2 - js basic - Objects Of Objects

---
+++ JavaScript101/ObjectsofObjects.js
@@ -0,0 +1,11 @@
+const user1 = {
+	name: "Niall",
+	age: 22,
+	address: {
+		city: "Pune",
+		state: "Maharashtra",
+		country: "India"
+	}
+};
+
+console.log(user1.name + " is " + user1.age + " years old and he lives in " + user1.address.city + user1.address.state + user1.address.country);
