727f4cdeb3eba06a120deff23337bcadf161303f
niallBytes <unaniniall3@gmail.com>
2026-06-19 17:11:17 +0530

    1.2 - Js Basics - Arrays Of Objects

---
+++ JavaScript101/arraysOfObjects.js
@@ -0,0 +1,13 @@
+const users = [{
+    name: "Harkirat",
+    age: 21
+}, {
+    name: "raman",
+    age: 22
+}
+]
+
+const user1Name = users[0].name; 
+const user1Age = users[0].age;
+
+console.log(user1Name + user1Age)
